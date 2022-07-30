cf6b1758c0be4b7b2dd33ca26362d14d40aaea8c
Phan Huy Hoang <hoangph271@gmail.com>
2022-07-30 14:35:24 +0700

    [Add] OpenSea API trials

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,18 @@ import { Gallery } from './components/Gallery'
 
 import './App.css'
 
+// const options = { method: 'GET' }
+// fetch('https://api.opensea.io/api/v1/collection/doodles-official', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err))
+
+// const options = { method: 'GET', headers: { Accept: 'application/json' } }
+// fetch('https://api.opensea.io/api/v1/assets?collection=canon-ftb-ql&cursor=LXBrPTI3NjE3NDc5Ng==', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err))
+
 function App () {
   return (
     <div className="App">
